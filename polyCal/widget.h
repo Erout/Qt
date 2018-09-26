6182e3662ef89a6ef484b60a07984d96f93eba0a
Erout <31929622+Erout@users.noreply.github.com>
2018-09-27 00:10:03 +0800

    new

--- polyCal/widget.h
+++ polyCal/widget.h
@@ -14,9 +14,17 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
+private slots:
+	//加减乘求导带入值五个功能，其中，求导和带入值两个式子一起给出结果
+	void on_addBtn_clicked();
+	void on_subBtn_clicked();
+	void on_mulBtn_clicked();
+	void on_derBtn_clicked();
+	void on_InpBtn_clicked();
 
 private:
     Ui::Widget *ui;
+    
 };
 
 #endif // WIDGET_H
